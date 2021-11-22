6e2d3e6d1b7041d28db6a8019337946d870ee624
luisoropeza <luisdavid.ingsis@gmail.com>
2021-11-22 08:52:43 -0400

    algunos detalles

--- views/CostosPantalla3.js
+++ views/CostosPantalla3.js
@@ -22,7 +22,7 @@ export default function CostoPantalla3(props) {
 
     let { mes, rango } = FormManofactura;
 
-    mes = TableService.length
+    pos_mes = TableService.length
 
     let [service, setService] = React.useState("");
 
@@ -85,38 +85,8 @@ export default function CostoPantalla3(props) {
         }
     }
 
-    function addFecha(pos) {
-        let res = "";
-        if (pos == 0) {
-            res = "Para la fecha: Enero"
-        } else if (pos == 1) {
-            res = "Para la fecha: Febrero"
-        } else if (pos == 2) {
-            res = "Para la fecha: Marzo"
-        } else if (pos == 3) {
-            res = "Para la fecha: Abril"
-        } else if (pos == 4) {
-            res = "Para la fecha: Mayo"
-        } else if (pos == 5) {
-            res = "Para la fecha: Junio"
-        } else if (pos == 6) {
-            res = "Para la fecha: Julio"
-        } else if (pos == 7) {
-            res = "Para la fecha: Agosto"
-        } else if (pos == 8) {
-            res = "Para la fecha: Septiembre"
-        } else if (pos == 9) {
-            res = "Para la fecha: Octubre"
-        } else if (pos == 10) {
-            res = "Para la fecha: Noviembre"
-        } else if (pos == 11) {
-            res = "Para la fecha: Diciembre"
-        }
-        return res;
-    }
-
     function addMes(pos) {
-        let res = "";
+        let res = "fechas no disponibles";
         if (pos == 0) {
             res = "Enero"
         } else if (pos == 1) {
@@ -179,7 +149,7 @@ export default function CostoPantalla3(props) {
                             </Select>
                         </Stack>
                     </FormControl>
-                    <Center><Text bold>{addFecha(mes)}</Text></Center>
+                    <Center><Text bold>{addMes(pos_mes)}</Text></Center>
                     <Center>
                         <Button onPress={buttonPress}>Añadir</Button>
                     </Center>
