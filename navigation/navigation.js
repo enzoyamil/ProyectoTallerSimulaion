e4f9f2d022a4c2f26eee3e25ac0dec78fc113fcf
celier.choque <celier98jaime@gmail.com>
2021-12-17 12:37:55 -0400

    build files

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -27,12 +27,9 @@ import MargenBruto from "../views/MargenBruto";
 import DatosCredito from "../views/DatosCredito";
 import VanTir from "../views/VanTir";
 import Report from "../views/Report";
-<<<<<<< HEAD
 import Home from "../views/Home";
-=======
 import { ReporteContext } from "../components/ReporteContext";
 import {getReporte,iniReporte,setReporteLocal,clearReporte} from "../helpers/ReportLocalFunciones"
->>>>>>> 1eadcf56edc6a839f05f7ec716ac7afce4da65a1
 
 const Stack = createNativeStackNavigator();
 
@@ -53,38 +50,6 @@ function navigation() {
 
   
   return (
-<<<<<<< HEAD
-    <Stack.Navigator>
-      <Stack.Screen name="FOCASE" component={Home} />
-      <Stack.Screen name="Información Personal" component={pantalla1} />
-      <Stack.Screen name="Informacón del Emprendimiento" component={pantalla2} />
-      {/* <Stack.Screen name="Descripción del Negocio" component={pantalla3} /> */}
-      <Stack.Screen options={{ title: "Hoja de costos" }} name="Hoja-de-Costos" component={CostoPantalla1} />
-      <Stack.Screen options={{ title: "Hoja de costos" }} name="Hoja-de-Costos2" component={CostoPantalla2} />
-      <Stack.Screen options={{ title: "Hoja de costos" }} name="Hoja-de-Costos3" component={CostoPantalla3} />
-      <Stack.Screen options={{ title: "Hoja de costos" }} name="Hoja-de-Costos4" component={CostoPantalla4} />
-      <Stack.Screen options={{ title: "Hoja de costos" }} name="Hoja-de-Costos4_2" component={CostoPantalla4_2} />
-      {/* <Stack.Screen name="Descripción del Servicio" component={pantalla4} />  */}
-      {/* <Stack.Screen name="Analisis Mercado" component={pantalla5} />  */}
-      {/* <Stack.Screen name="Estrategia de mercado" component={pantalla6} /> */}
-      <Stack.Screen options={{ title: 'Costos Operativos' }} name="Costos Operativos" component={CostosOperativos} />
-      <Stack.Screen name="Presupuesto Emprendimiento" component={pantallaPresupuesto} />
-      <Stack.Screen name="Mano Emprendedor" component={pantallaManoEmprendedor} />
-      <Stack.Screen name="Materia Prima" component={materiaPrima} />
-      <Stack.Screen name="Requerimientos Promocionales" component={reqPromo} />
-      <Stack.Screen name="Gastos Operativos" component={gastosOperativos} />
-      <Stack.Screen name="Infraestructura" component={infraestructura} />
-      <Stack.Screen name="Maquinaria" component={maquinaria} />
-      <Stack.Screen name="Requerimiento Legal" component={reqLegales} />
-      <Stack.Screen name="Presupuesto Resumen" component={presupuestoResumen} />
-      <Stack.Screen name="Plan Inversión" component={planInversion} />
-      <Stack.Screen options={{ title: "Analisis Financiero" }} name="MargenBruto" component={MargenBruto} />
-      <Stack.Screen options={{ title: "Datos del Credito" }} name="DatosCredito" component={DatosCredito} />
-      <Stack.Screen name="Datos Resumen" component={datosResumen} />
-      <Stack.Screen name="Resultados" component={VanTir} />
-      <Stack.Screen name="Reporte" component={Report} />
-    </Stack.Navigator>
-=======
     <ReporteContext.Provider value={[reporte, setReporte]}>
       <Stack.Navigator>
         <Stack.Screen name="Información Personal" component={pantalla1} />
@@ -116,7 +81,6 @@ function navigation() {
         <Stack.Screen name="Reporte" component={Report} />
       </Stack.Navigator>
     </ReporteContext.Provider>
->>>>>>> 1eadcf56edc6a839f05f7ec716ac7afce4da65a1
   );
 }
 
