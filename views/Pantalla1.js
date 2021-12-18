29eaf741a59be3b8d6bd214d420de04897281ef5
Enzo Canedo <enzo.canedo7@gmail.com>
2021-12-17 21:37:59 -0400

    commit

--- views/Pantalla1.js
+++ views/Pantalla1.js
@@ -22,39 +22,31 @@ function Pantalla1(props) {
         setFormPersonal({ ...FormPersonal, [input]: value });
     }
     function buttonPress() {
-
-        // if (name == '' || apellido == '' || ci == '' || extension == '' || edad == '' || telefono == '' || direccion == '') {
-        //     console.log(name);
-        //     Alert.alert("Error Campo Vacio");
-        // } else if (tamanioMaximo(name, 15)) {
-        //     Alert.alert("Cadena nombre muy grande");
-        // } else if (tamanioMin(name, 2)) {
-        //     Alert.alert("Cadena nombre muy pequeña");
-        // } else if (tamanioMaximo(apellido, 15)) {
-        //     Alert.alert("Cadena apellido muy grande");
-        // } else if (tamanioMaximo(apellido, 15)) {
-        //     Alert.alert("Cadena apellido muy grande");
-        // } else if (tamanioMin(apellido, 4)) {
-        //     Alert.alert("Cadena apellido muy pequeña");
-        // } else if (edad < 0 || edad < 18) {
-        //     Alert.alert("Edad no aceptada");
-        // } else if (telefono.length < 7) {
-        //     Alert.alert("Telefono no aceptado");
-        // } else if (ci.length < 7) {
-        //     Alert.alert("CI no aceptado");
-        // } else if (tamanioMaximo(direccion, 50)) {
-        //     Alert.alert("Edad no aceptada");
-        // } else if (tamanioMin(direccion, 10)) {
-        //     Alert.alert("Dirección muy corta");
-        // } else {
-        //     Alert.alert("Succesfull");
-        //     navigation.navigate("Información del Emprendimiento");
-        // }
-        setReporte((obj)=>({...obj,nombre_propietario:FormPersonal.name}));
-        console.log(reporte,"pantalla1");
-        navigation.navigate("Datos del Emprendimiento");
+        if (name == '' || apellido == '' || ci == '' || extension == '' || edad == '' || telefono == '' || direccion == '') {
+            Alert.alert("Error Campo Vacio");
+        } else if (tamanioMaximo(name, 15)) {
+            Alert.alert("Cadena nombre muy grande");
+        } else if (tamanioMin(name, 2)) {
+            Alert.alert("Cadena nombre muy pequeña");
+        } else if (tamanioMaximo(apellido, 15)) {
+            Alert.alert("Cadena apellido muy grande");
+        } else if (tamanioMin(apellido, 2)) {
+            Alert.alert("Cadena apellido muy pequeña");
+        } else if (edad < 0 || edad < 18) {
+            Alert.alert("Edad no aceptada");
+        } else if (telefono.length < 8) {
+            Alert.alert("Teléfono no aceptado");
+        } else if (ci.length < 10) {
+            Alert.alert("CI no aceptado");
+        } else if (tamanioMaximo(direccion, 50)) {
+            Alert.alert("Edad no aceptada");
+        } else if (tamanioMin(direccion,5)) {
+            Alert.alert("Dirección muy corta");
+        } else {
+            setReporte((obj)=>({...obj,nombre_propietario:FormPersonal.name}));
+            navigation.navigate("Datos del Emprendimiento");
+        }   
     }
-
     let { name, apellido, ci, extension, edad, telefono, direccion } = FormPersonal;
     let [service, setService] = React.useState("");
 
@@ -80,7 +72,7 @@ function Pantalla1(props) {
                             <FormControl.Label>CI(*)</FormControl.Label>
                             <Input variant="rounded" value={ci} onChangeText={(value) => EstadoInputs(value, 'ci')} keyboardType="numeric" />
                             <FormControl.Label>Extensión(*)</FormControl.Label>
-                            <Select placeholder="Extesion" variant="rounded" value={extension}
+                            <Select placeholder="Extesión" variant="rounded" value={extension}
                                 selectedValue={service} onValueChange={(itemValue) => setService(itemValue)}
                                 onValueChange={(value) => EstadoInputs(value, 'extension')}>
                                 <Select.Item label="cbba" value="cochabamba" />
@@ -95,7 +87,7 @@ function Pantalla1(props) {
                             </Select>
                             <FormControl.Label>Edad(*)</FormControl.Label>
                             <Input keyboardType='numeric' variant="rounded" value={edad} onChangeText={(value) => EstadoInputs(value, 'edad')} />
-                            <FormControl.Label>Telefono(*)</FormControl.Label>
+                            <FormControl.Label>Teléfono(*)</FormControl.Label>
                             <Input keyboardType='numeric' variant="rounded" value={telefono} onChangeText={(value) => EstadoInputs(value, 'telefono')} />
                             <FormControl.Label>Dirección(*)</FormControl.Label>
                             <Input variant="rounded" value={direccion} onChangeText={(value) => EstadoInputs(value, 'direccion')} />
